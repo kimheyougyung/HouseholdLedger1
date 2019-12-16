f11b855b36562ecb347471bc267e559889bf20d2
kimheyongyung <dmlgus0607@naver.com>
2019-12-17 03:06:09 +0900

    플로팅액션버튼 추가 // 네비게이션 컨트롤러 오류 뜸

--- app/src/main/java/com/example/householdledger2/MainActivity.java
+++ app/src/main/java/com/example/householdledger2/MainActivity.java
@@ -20,37 +20,104 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class MainActivity extends AppCompatActivity {
+//AppCompatActivity 클래스는 과거 안드로이드 버전과의 호환성을 유지하면서
+//  새로운 버전의 기능도 사용할 수 있게 한다.
+    private AppBarConfiguration mAppBarConfiguration; // 앱바 위치배치?
 
-    private AppBarConfiguration mAppBarConfiguration;
+    FloatingActionButton fab_add, fab_memo, fab_spend;
+    Animation FabOpen, FabClose, FabClockwise, Fabanticlockwise;
+    boolean isOpen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_main); //setContentView - xml 에 정의되있는 View 들을 메모리 상에 객체화시켜줌
+
+
+        fab_add = (FloatingActionButton)findViewById(R.id.fab_add);
+        fab_memo = (FloatingActionButton)findViewById(R.id.fab_memo);
+        fab_spend = (FloatingActionButton)findViewById(R.id.fab_spend);
+        FabOpen = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
+        FabClose = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close);
+        FabClockwise = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_clockwise);
+        Fabanticlockwise = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.retate_anticlockwise);
+        fab_add.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                if (isOpen)
+                {
+                    fab_spend.startAnimation(FabClose);
+                    fab_memo.startAnimation(FabClockwise);
+                    fab_add.startAnimation(Fabanticlockwise);
+                    fab_memo.setClickable(false);
+                    fab_spend.setClickable(false);
+                    isOpen = false;
+                }
+                else
+                {
+                    fab_spend.startAnimation(FabOpen);
+                    fab_memo.startAnimation(FabOpen);
+                    fab_add.startAnimation(Fabanticlockwise);
+                    fab_memo.setClickable(true);
+                    fab_spend.setClickable(true);
+                    isOpen = true;
+
+                }
             }
         });
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar); // 툴바설정
+        setSupportActionBar(toolbar); // 액션바와 같게 만들어줌
+
+        // FloatingActionButton 플로팅액션버튼 "+"  fab 선언함
+        fab_add.setOnClickListener(new View.OnClickListener() { //  fab 클릭하였을 때
+            @Override
+            public void onClick(View view) {
+                if (isOpen)
+                {
+                    fab_spend.startAnimation(FabClose);
+                    fab_memo.startAnimation(FabClockwise);
+                    fab_add.startAnimation(Fabanticlockwise);
+                    fab_memo.setClickable(false);
+                    fab_spend.setClickable(false);
+                    isOpen = false;
+                }
+                else
+                {
+                    fab_spend.startAnimation(FabOpen);
+                    fab_memo.startAnimation(FabOpen);
+                    fab_add.startAnimation(Fabanticlockwise);
+                    fab_memo.setClickable(true);
+                    fab_spend.setClickable(true);
+                    isOpen = true;
+
+                }
+
+//                Snackbar.make(view, "스낵바 출력", Snackbar.LENGTH_LONG).show();
+////                Snackbar.make(view, "스낵바 출력", Snackbar.LENGTH_LONG).setAction
+////                        ("Action", null).show();
+//                // 스낵바 출력
+            }
+        });
+        DrawerLayout drawer = findViewById(R.id.activity_layout); // 드로어 레이아웃, 메인 액티비티레이아웃
+        NavigationView navigationView = findViewById(R.id.nav_view); // 네비게이션 드로어
+
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home,
                 R.id.nav_calculator, R.id.nav_stats,
                 R.id.nav_tools, R.id.nav_share, R.id.nav_setting)
-                .setDrawerLayout(drawer)
+                .setDrawerLayout(drawer)  // 드로어 레이아웃을 가져오다.
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment); // 이게 왜그런건지모르겟음
+//        // NavController 네비게이션 컨트롤러 역할한다.
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+//        //
         NavigationUI.setupWithNavController(navigationView, navController);
     }
 
